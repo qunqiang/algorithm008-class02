8e5c6136972a83b1bd4a09bbd316dddf833edee4
王群强 <wangqunqiang@xiaozhu.com>
2020-04-13 20:01:10 +0800

    day1 movezero

---
+++ Week_01/leetcode/movezero.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+//
+// 示例:
+//
+// 输入: [0,1,0,3,12]
+//输出: [1,3,12,0,0]
+//
+// 说明:
+//
+//
+// 必须在原数组上操作，不能拷贝额外的数组。
+// 尽量减少操作次数。
+//
+// Related Topics 数组 双指针
+
+func main() {
+	var nums = []int{0, 1, 0, 3, 12}
+	moveZeroes(nums)
+	fmt.Println("after:", nums)
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func moveZeroes(nums []int) {
+	l := len(nums)
+	c := 0
+	for i := 0; i < l; i++ {
+		if nums[i] == 0 {
+			c++
+		} else {
+			if c > 0 {
+				nums[i-c], nums[i] = nums[i], 0
+			}
+		}
+	}
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
